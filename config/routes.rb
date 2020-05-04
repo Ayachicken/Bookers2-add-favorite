958c167e6b2e4f99e08c910361256af8de771065
Ayachicken <digimonmon.a@gmail.com>
2020-05-05 02:35:52 +0900

    コメント機能実装途中

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  devise_for :users
+  get '/home/top' => 'home#top'
+  get '/home/about' => 'home#about'
+  root 'home#top'
+  resources :users,only: [:show,:index,:edit,:update]
+  resources :books do
+    resource :book_comments,only: [:create,:destroy]
+  end
+end
