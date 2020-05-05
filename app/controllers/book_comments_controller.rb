c3658dfddc9afa3fc22c6b5d34105965d1202475
Ayachicken <digimonmon.a@gmail.com>
2020-05-05 20:38:49 +0900

    [finish]課題３

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,10 +1,14 @@
 class BookCommentsController < ApplicationController
     def create
         @book = Book.find(params[:book_id])
-        @comment = current_user.book_comments.new(book_comment_params)
-        @comment.book_id = @book.id
-        @comment.save
-        redirect_to book_path(@book)
+        @book_comment = current_user.book_comments.new(book_comment_params)
+        @book_comment.book_id = @book.id
+        if @book_comment.save
+            redirect_to book_path(@book)
+        else
+            @book_comments = @book.book_comments
+            render '/books/show'
+        end
     end
 
     def destroy
